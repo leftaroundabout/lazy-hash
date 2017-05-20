84c5e25065184f6350ed205076e98032ed3a4790
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-05-20 18:20:08 +0200

    Pre-hashed values.

--- Data/LazyHash/Class.hs
+++ Data/LazyHash/Class.hs
@@ -12,7 +12,9 @@
 {-# LANGUAGE CPP                   #-}
 {-# LANGUAGE UndecidableInstances  #-}
 {-# LANGUAGE ConstraintKinds       #-}
+{-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE TemplateHaskell       #-}
 
 module Data.LazyHash.Class where
 
@@ -29,6 +31,9 @@ import Control.Monad.Constrained hiding (forM)
 
 import qualified Data.Hashable as SH
 
+import Language.Haskell.TH
+import Language.Haskell.TH.Quote
+import Language.Haskell.Meta.Parse
 
 class Hash h where
   defaultSalt :: h
@@ -44,16 +49,40 @@ class Hash h => Hashable h a where
   hash = hashWithSalt defaultSalt
 
 instance Hashable Int Int where hashWithSalt = SH.hashWithSalt
+instance Hashable Int String where hashWithSalt = SH.hashWithSalt
 
 
-data LHashableFunction h a b = LHashableFunction {
-    functionHash :: h
-  , lhashableFunction :: a -> b
+data Prehashed h a = Prehashed {
+    lazyHash :: !h
+  , prehashedValue :: a
   }
 
-type Hash' h = (Hashable h h, Hashable h String)
+newtype LazilyHashableFunction h a b = LHF {
+    getLHF :: Prehashed h (a->b)
+  }
 
-instance Hash' h => Category (LHashableFunction h) where
-  id = LHashableFunction defaultSalt id
-  LHashableFunction hf f . LHashableFunction hg g
-       = LHashableFunction (hashWithSalt hf hg) (f . g)
+type Hash' h = (Hashable h h, Hashable h String, Num h)
+
+instance Hash' h => Category (LazilyHashableFunction h) where
+  id = lhf defaultSalt id
+  LHF (Prehashed hf f) . LHF (Prehashed hg g)
+       = lhf (hashWithSalt hf hg) (f . g)
+
+lhf :: h -> (a->b) -> LazilyHashableFunction h a b
+lhf h = LHF . Prehashed h
+
+
+-- | Transform an ordinary value into a pre-hashed one. This hashes the /source code
+--   contained in the quasi quote/, assuming that the behaviour of anything invoked
+--   therein will never change.
+-- 
+--   Applying this to anything but named, fixed-predefined values (standard library
+--   functions etc.) is probably a bad idea.
+fundamental :: QuasiQuoter
+fundamental = QuasiQuoter (return . fund) undefined undefined undefined
+ where fund v = AppE (AppE (VarE 'Prehashed)
+                 (LitE . IntegerL . fromIntegral $ (hash v :: Int)))
+                 vParsed
+        where vParsed = case parseExp v of
+                  Right exp -> exp
+                  Left perr -> error perr
