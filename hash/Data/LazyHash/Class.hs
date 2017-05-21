df8155f0ef7653b5f490da627261f2b0c2ea378b
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-05-22 00:17:51 +0200

    Most of the important `base` instances for `Hashable`.

--- hash/Data/LazyHash/Class.hs
+++ hash/Data/LazyHash/Class.hs
@@ -20,19 +20,31 @@ module Data.LazyHash.Class where
 
 
 import qualified Data.Hashable as SH
+import Data.Bits (xor)
 
 import Language.Haskell.TH
 import Language.Haskell.TH.Quote
 import Language.Haskell.Meta.Parse
 
 import Data.Void
+import Data.Foldable
 
 class Hash' h where
+  zeroHash :: h
+  distinguisher :: h
   defaultSalt :: h
+  combine :: h -> h -> h
 instance Hash' Int where
+  zeroHash = 0
+  {-# INLINE zeroHash #-}
+  distinguisher = fromIntegral $ (maxBound :: Word) `quot` 3
+  {-# INLINE distinguisher #-}
   defaultSalt = -2578643520546668380
+  {-# INLINE defaultSalt #-}
     -- same as http://hackage.haskell.org/package/hashable-1.2.6.0/docs/src/Data-Hashable-Class.html#hashWithSalt
     -- (on 64-bit)
+  combine h₀ h₁ = (h₀ * 16777619) `xor` h₁
+  {-# INLINE combine #-}
 
 infixl 6 #
 class Hash' h => Hashable h a where
@@ -42,10 +54,33 @@ class Hash' h => Hashable h a where
   hash :: a -> h
   hash = (#) defaultSalt
 
-instance Hashable Int Int where (#) = SH.hashWithSalt
-instance Hashable Int String where (#) = SH.hashWithSalt
-instance Hashable Int () where (#) = SH.hashWithSalt
-instance Hashable Int Void where (#) = SH.hashWithSalt
+instance Hashable Int Int where hash = SH.hash; (#) = SH.hashWithSalt
+instance Hashable Int () where hash = SH.hash; (#) = SH.hashWithSalt
+instance Hashable Int Void where hash = SH.hash; (#) = SH.hashWithSalt
+instance Hashable Int Integer where hash = SH.hash; (#) = SH.hashWithSalt
+instance Hashable Int Double where hash = SH.hash; (#) = SH.hashWithSalt
+instance Hashable Int Float where hash = SH.hash; (#) = SH.hashWithSalt
+instance Hashable Int Bool where hash = SH.hash; (#) = SH.hashWithSalt
+instance Hashable Int Char where hash = SH.hash; (#) = SH.hashWithSalt
+
+instance Hashable h a => Hashable h [a] where (#) = foldl' (#)
+
+-- http://hackage.haskell.org/package/hashable-1.2.6.0/docs/src/Data-Hashable-Class.html#line-481
+instance Hashable h a => Hashable h (Maybe a) where
+  hash Nothing = zeroHash
+  hash (Just a) = distinguisher # a
+  h # Nothing = h `combine` zeroHash
+  h # Just a = h `combine` distinguisher # a
+
+instance (Hashable h a, Hashable h b) => Hashable h (a,b) where
+  hash (a,b) = hash a # b
+  h # (a,b) = h # a # b
+
+instance (Hashable h a, Hashable h b) => Hashable h (Either a b) where
+  hash (Left a)  = zeroHash # a
+  hash (Right b) = distinguisher # b
+  s # Left a = s `combine` zeroHash # a
+  s # Right b = s `combine` distinguisher # b
 
 
 data Prehashed h a = Prehashed {
