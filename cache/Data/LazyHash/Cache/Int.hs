4095bba7c59a54d39b0c6a96649bc6c4310db1f3
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-05-22 12:14:32 +0200

    An Int-specialised version of the cache module.
    This is not suitable for serious applications, but handy enough for quick
    experimental stuff.

---
+++ cache/Data/LazyHash/Cache/Int.hs
@@ -0,0 +1,49 @@
+-- |
+-- Module      : Data.LazyHash.Cache.Int
+-- Copyright   : (c) Justus Sagemüller 2017
+-- License     : GPL v3
+-- 
+-- Maintainer  : (@) jsagemue $ uni-koeln.de
+-- Stability   : experimental
+-- Portability : portable
+-- 
+-- Identical interface to "Data.LazyHash.Cache", but specialised to hashes of type 'Int'
+-- (which are computed with "Data.Hashable"). At only 64 bits, collisions are
+-- <https://en.wikipedia.org/wiki/Birthday_problem likely> with this type once you store
+-- a considerable number of values, so don't use it for serious applications.
+
+{-# LANGUAGE LambdaCase #-}
+
+module Data.LazyHash.Cache.Int where
+
+import Data.LazyHash.Cache (cachedValueInFile)
+
+import Data.LazyHash.Class
+import Data.LazyHash.Numerical ()
+
+import qualified Data.Hashable as SH
+
+import Data.Binary
+import System.FilePath
+import System.Directory
+
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy as BS (toStrict)
+
+import Numeric (showHex) 
+import Data.Word (Word64) 
+
+cached :: Binary a => Prehashed Int a -> IO a
+cached = cachedWithin ".hscache/lazy-hashed"
+
+cachedTmp :: Binary a => Prehashed Int a -> IO a
+cachedTmp v = do
+   tmpRoot <- getTemporaryDirectory
+   cachedWithin (tmpRoot</>"hs-lazy-hashed") v
+
+cachedWithin :: Binary a => FilePath        -- ^ Storage directory
+                         -> Prehashed Int a -- ^ Value to cache
+                         -> IO a
+cachedWithin path (Prehashed h v) = do
+   let fname = path </> showHex (fromIntegral h :: Word64) [] <.> ".lhbs"
+   cachedValueInFile fname v
